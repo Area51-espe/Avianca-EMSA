8f145d840f3a418f34328b5c286663fe71fef85b
ByronAbata <bayronrocked97@gmail.com>
2025-07-24 09:44:37 -0500

    login-registro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
+import 'home.dart';
+import 'register.dart'; // <-- Asegúrate de importar tu archivo de registro
 
 void main() {
   runApp(const MyApp());
@@ -10,46 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Proyecto Final', // Changed the app title
+      title: 'Proyecto Final',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Proyecto Final'), // Changed the home page title
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget { // Changed to StatelessWidget
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title), // Uses the title passed to the widget
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Icon(
-              Icons.home, // Home icon
-              size: 100.0, // Adjust size as needed
-              color: Colors.deepPurple, // Adjust color as needed
-            ),
-            const SizedBox(height: 20), // Adds some space between the icon and text
-            Text(
-              'Proyecto Final', // The desired text
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      // Removed the floatingActionButton
+      initialRoute: '/login', // Ruta inicial
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+        '/register': (context) => RegisterScreen(), // <-- Agregada aquí
+      },
     );
   }
 }
